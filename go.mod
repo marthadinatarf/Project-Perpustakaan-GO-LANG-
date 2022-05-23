af17dc252b1b6742944b24530b9ea2e5be675bdb
Rizki Firdaus <rizki.achanation@gmail.com>
2022-05-23 09:40:29 +0700

    add fitur/book

--- go.mod
+++ go.mod
@@ -1,3 +1,19 @@
 module perpusgo
 
 go 1.18
+
+require gorm.io/gorm v1.23.5
+
+require (
+	github.com/mattn/go-colorable v0.1.11 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
+	golang.org/x/sys v0.0.0-20211103235746-7861aae1554b // indirect
+)
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.4 // indirect
+	github.com/labstack/gommon v0.3.1
+)
--- repository/book/book.go
+++ repository/book/book.go
@@ -1,1 +1,58 @@
 package book
+
+import (
+	"errors"
+	"perpusgo/entity"
+
+	"github.com/labstack/gommon/log"
+	"gorm.io/gorm"
+)
+
+func New(db *gorm.DB) *BookRepo {
+	return &BookRepo{
+		Db: db,
+	}
+}
+
+type BookRepo struct {
+	Db *gorm.DB
+}
+
+// Func InsertBook untuk menambah buku
+func (br *BookRepo) InsertBook(newBook entity.Book) (entity.Book, error) {
+	if err := br.Db.Create(&newBook).Error; err != nil {
+		log.Warn(err)
+		return entity.Book{}, errors.New("tidak bisa insert data")
+	}
+	log.Info()
+	return newBook, nil
+}
+
+// Func SelectBook untuk menampilkan seluruh buku
+func (br *BookRepo) SelectBook() ([]entity.Book, error) {
+	arrBook := []entity.Book{}
+
+	if err := br.Db.Find(&arrBook).Error; err != nil {
+		log.Warn(err)
+		return nil, errors.New("tidak bisa menampilkan data seluruh buku")
+	}
+
+	if len(arrBook) == 0 {
+		log.Warn("tidak ada data")
+		return nil, errors.New("tidak ada data")
+	}
+	log.Info()
+	return arrBook, nil
+}
+
+// Func UpdateBook untuk mengupdate buku sesuai ID buku
+func (br *BookRepo) UpdateBook(id int, update entity.Book) (entity.Book, error) {
+	var book entity.Book
+
+	if err := br.Db.Updates(&update).Where("id = ?", id).Find(&book).Error; err != nil {
+		log.Warn(err)
+		return entity.Book{}, errors.New("tidak bisa update buku")
+	}
+	log.Info()
+	return book, nil
+}
